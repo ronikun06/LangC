b5f54465695e5aeacda725c1c44848be957820a8
Yuta Shoho <d2531031@eng.kitakyu-u.ac.jp>
2023-11-17 14:32:20 +0900

    1-commit

---
+++ 2022531031.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main(){
+    int x,y,z;
+    int price
+    printf("Welcome to C Programming!\n");
+x=10;
+y=2*x;
+z=x+y;
+printf("x=%d,y=%d,z=%d\n",x,y,z);
+price=500;
+price=float(price*1.1)
+}
---
+++ main2.c
@@ -0,0 +1,8 @@
+#include<stdio.h>
+int main(){
+    int price;
+    price=10;
+    printf("%d円見つけた\n",price);
+    printf("%d+%d=%d\n",1000,2000,3000);
+    return 0;
+}
