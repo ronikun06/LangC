70b4fa35d2c32fb9684cf900b60242c7316f7d05
Yuta Shoho <d2531031@eng.kitakyu-u.ac.jp>
2023-11-24 14:19:55 +0900

    第3回目特別授業講義内容

--- vol2/2022531031.c
+++ vol2/2022531031.c
@@ -1,7 +1,7 @@
 #include<stdio.h>
 int main(){
     int x,y,z;
-    int price
+    int price;
     printf("Welcome to C Programming!\n");
 x=10;
 y=2*x;
@@ -9,4 +9,5 @@ z=x+y;
 printf("x=%d,y=%d,z=%d\n",x,y,z);
 price=500;
 price=float(price*1.1)
+printf("%f",price)
 }
